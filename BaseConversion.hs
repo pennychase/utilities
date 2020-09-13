d567a6d9c31577cbc8822068d25cc4cc694afdf4
Penny Chase <mpchase@mac.com>
2020-09-13 15:26:19 -0400

    Added additional comments and moved showDigit into getDigits

--- BaseConversion.hs
+++ BaseConversion.hs
@@ -6,9 +6,14 @@ import Data.List(lookup)
 import Numeric (showIntAtBase)
 import Data.Char (intToDigit)
 
--- Conversion to binary, octal, hexadecimal, decimal
--- For the number to be converted, write as a biary, octal,
--- hexadecimal, or decimal literal with 0b, 0O, 0x prefixes
+-- This module has functions to convert between binary, octal, hexadecimal, decimal
+-- The input is a number (constrained to be Integral). For binary, octal, and
+-- hexadecimal use the 0b, 0O, and 0x prefixes.
+-- The output is a string representing the digits of the converted number so we
+-- can use 'a' through 'f' in hexadecimal numbers.
+-- Since the output string uses the prefixes for binary, octal, and haxadecial,
+-- we can turn into a number with read (doesn't seem to work for binary literals)
+
 toBinary :: Integral a => a -> String
 toBinary = convertToBase 2
 
@@ -24,20 +29,26 @@ toHex = convertToBase 16
 prefix :: Integral a => a -> String
 prefix base =
   case base of
-    2 -> "0b"
-    8 -> "0O"
-    16 -> "0x"
+    2         -> "0b"
+    8         -> "0O"
+    16        -> "0x"
     otherwise -> ""
 
+-- Convert a number to its representation in the specified base, and display as
+-- a String with the base prefix ("Ob", "0O", "0x").
 convertToBase :: Integral a => a -> a -> String
-convertToBase base num = (prefix base) ++ (map showDigit $ getDigits base num [])
+convertToBase base num = prefix base ++ getDigits base num []
 
-getDigits :: Integral a => a -> a -> [a] -> [a]
+-- Return the list of digits to represent a number in the specified base.
+-- Use an accumulator to avoid reversing the list.
+getDigits :: Integral a => a -> a -> String -> String
 getDigits b 0 acc = acc
 getDigits b n acc =
   let (q,r) = divMod n b
-  in (getDigits b q (r:acc))
+  in getDigits b q ((showDigit r):acc)
 
+-- Convert a numeric "digit" (the quotient from divMod) to a Char. 10 through 15 are
+-- translated into 'a' through 'f'
 showDigit :: Integral a => a -> Char
 showDigit d =
   case lookup d mapping of
@@ -46,7 +57,8 @@ showDigit d =
   where
     mapping = zip [0..15] $ (map (head . show) [0..9]) ++ ['a'..'f']
 
--- Use functions from Numeric and Data.Char
+
+-- As an alternative, use functions from Numeric and Data.Char to convert to Base
 
 convertToBase' :: (Integral a, Show a) => a -> a -> String
 convertToBase' b n =
